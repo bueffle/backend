5c368a36098e0b7e3a6da5c5f473d3640df230a9
Justin Isler <justin.isler@gmail.com>
2019-11-20 21:06:29 +0100

    Add dummy data via sql and add index.hmtl

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO card (question,answer) VALUES ('question 1', 'answer to 1');
+INSERT INTO card (question,answer) VALUES ('question 2', 'answer to 2');
+INSERT INTO card (question,answer) VALUES ('question 3', 'answer to 3');
+INSERT INTO card (question,answer) VALUES ('question 4', 'answer to 4');
+INSERT INTO card (question,answer) VALUES ('question 5', 'answer to 5');
+INSERT INTO card (question,answer) VALUES ('question 6', 'answer to 6');
+INSERT INTO card (question,answer) VALUES ('question 7', 'answer to 7');
+INSERT INTO card (question,answer) VALUES ('question 8', 'answer to 8');
+INSERT INTO card (question,answer) VALUES ('question 9', 'answer to 9');
+INSERT INTO card (question,answer) VALUES ('question 10', 'answer to 10');
