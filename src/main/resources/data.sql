e6fd945be5cca64c2f7474979d2474779811e60b
Linda <linda.geissmann@students.ffhs.ch>
2019-11-27 20:07:07 +0100

    first JQuery for collection.html: edit name, description index.html: collection descriptions by GET-Request

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,4 +8,10 @@ INSERT INTO card (question,answer) VALUES ('question 7', 'answer to 7');
 INSERT INTO card (question,answer) VALUES ('question 8', 'answer to 8');
 INSERT INTO card (question,answer) VALUES ('question 9', 'answer to 9');
 INSERT INTO card (question,answer) VALUES ('question 10', 'answer to 10');
+INSERT INTO collection (name,description) VALUES ('Sammlung 1', 'Dies ist die Beschreibung der Sammlung 1.');
+INSERT INTO collection (name,description) VALUES ('Sammlung 2', 'Dies ist die Beschreibung der Sammlung 2.');
+INSERT INTO collection (name,description) VALUES ('Sammlung 3', 'Dies ist die Beschreibung der Sammlung 3.');
+INSERT INTO collection (name,description) VALUES ('Sammlung 4', 'Dies ist die Beschreibung der Sammlung 4.');
+INSERT INTO collection (name,description) VALUES ('Sammlung 5', 'Dies ist die Beschreibung der Sammlung 5.');
+INSERT INTO collection (name,description) VALUES ('Sammlung 6', 'Dies ist die Beschreibung der Sammlung 6.');
 
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,1 +1,98 @@
+$(function() {
+    getCollections();
+    $("#editCollectionBtn").on("click", editCollection);
 
+});
+
+/**
+ * Edit button was clicked: make collection editable, change buttons
+ */
+function editCollection() {
+    $(this).hide();
+    $("#collectionTitle").attr("contenteditable", "true");
+    $("#collectionTitle").css("background-color", "white");
+    $("#collectionDescription").attr("contenteditable", "true");
+    $("#collectionDescription").css("background-color", "white");
+    $("#collectionBtn1").html("Save");
+    $("#collectionBtn2").html("Cancel");
+    //cancel
+    $("#collectionBtn2").on("click", setCollectionUneditable);
+    //save
+    $("#collectionBtn1").on("click", function() {
+        setCollectionUneditable();
+        //todo: save changes via POST/PUT
+    });
+}
+
+/**
+ * Set elements of collection.html from edit mode back to normal mode
+ */
+function setCollectionUneditable() {
+    $("#editCollectionBtn").show();
+    $("#collectionTitle").removeAttr('contenteditable style');
+    $("#collectionDescription").removeAttr('contenteditable style');
+    //todo: buttons
+    $("#collectionBtn1").html("New Card");
+    $("#collectionBtn2").html("Start Training");
+}
+
+/**
+ * Get collections by ajax call
+ */
+function getCollections() {
+    var url = $(location).attr('origin'); //base url, instead of href
+    console.log("url: " + url);
+
+    $.ajax({
+        url: url + "/collections",
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            console.log("first value: " + data[0].name);
+            loadCollections(data);
+        }
+    });
+
+    /* Alternative:
+    $.get( url + "/collections", function(data) {
+        console.log(data[0].name);
+        loadCollections(data);
+    });*/
+}
+
+/**
+ * Load data of collections (response from GET to /collections) into view
+ * @param data
+ */
+function loadCollections(data) {
+    var collectionDescriptions = document.getElementsByClassName("card-text");
+    for (num in data) {
+        //parent
+        var row = $(".row");
+
+        //appendChild with JQuery?
+        /*row.append(
+              '<div class="col-md-4">'
+            +   '<div class="card mb-4 shadow-sm">'
+            +     '<img class="bd-placeholder-img card-img-top" src="https://images.unsplash.com/photo-1544642058-c5d172ab955c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" />'
+            +     '<div class="card-body">'
+            +       '<p class="card-text"' + '</p>'
+            //+       '<div class="d-flex justify-content-between align-items-center">'
+            //+         '<div class="btn-group">'
+            //+           '<a type="button" class="btn btn-sm btn-outline-secondary" value="Learn"</a>'
+            //+           '<a type="button" class="btn btn-sm btn-outline-secondary" value="Edit"</a>'
+            //+         '</div>'
+            //+         '<small class="text-muted"</small>'
+            //+       '</div>'
+            +     '</div>'
+            +   '</div>'
+            + '</div>'
+        );*/
+
+        //$(".card-text:eq(num)").innerText = (data[num].description);
+        //$(".card-text:eq(num)").html(data[num].description);
+        //$(".card-text:eq(num)").text(data[num].description);
+
+        collectionDescriptions[num].innerHTML = data[num].description;
+    }
+}
