5b7b93681219486a1313a72fbfd44e72f86d3bd6
Justin Isler <justin.isler@gmail.com>
2019-11-27 20:55:25 +0100

    Testing templating engines

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,1 +1,15 @@
+var collections=jQuery.get('/collections',function(data){console.log(data);})
 
+
+var source = document.getElementById("entry-template").innerHTML;
+var template = Handlebars.compile(source);
+
+
+for (var collection in collections ){
+
+    var context = { title: collection.name };
+    var html = template(context);
+
+
+    $( "#collections-content" ).append( html );
+}
