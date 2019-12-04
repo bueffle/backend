1f12267a570116296b433072a9182df4d82535f6
Justin Isler <justin.isler@gmail.com>
2019-12-04 20:46:51 +0100

    Add js helpers

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,1 +1,21 @@
+$( document ).ready(function() {
 
+    var titles = {"titles":[{"title":"tuk,asdfas jnw"},{"title":"tuk,asdfas jnw"},{"title":"tuk,asdfas jnw"},{"title":"tuk,asdfas jnw"},{"title":"tuk,asdfas jnw"},{"title":"tuk,asdfas jnw"},{"title":"tuk,asdfas jnw"},{"title":"tuk,asdfas jnw"}]};
+
+    //var titles =[{"title":"tuk,asdfas jnw"},{"title":"tuk,asdfas jnw"},{"title":"tuk,asdfas jnw"},{"title":"tuk,asdfas jnw"},{"title":"tuk,asdfas jnw"},{"title":"tuk,asdfas jnw"},{"title":"tuk,asdfas jnw"},{"title":"tuk,asdfas jnw"}];
+
+
+
+    var template = Handlebars.compile($('#entry-template').html());
+
+    var html = template(titles);
+
+    $('#collections-content').append(html);
+
+    testFunction();
+});
+
+
+function testFunction() {
+    console.log("test");
+}
