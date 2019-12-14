9f24bafef287072f5655d57db5fa8f2547ab907c
Linda <linda.geissmann@students.ffhs.ch>
2019-12-14 22:10:49 +0100

    delete card

--- src/main/resources/static/js/collections.js
+++ src/main/resources/static/js/collections.js
@@ -157,9 +157,10 @@ function deleteCard(card_id) {
         dataType: 'json',
         success: function(data) {
             console.log("data of deleteCard(): " + data);
-            renderCards(data);
+            //renderCards(data);
         }
     });
+    location.reload();
 }
 
 function appendToBodyCards(index, card) {
@@ -185,7 +186,7 @@ function clearCards() {
 }
 
 $('#create_new_card_submit').click(function(event) {
-    createCard()
+    createCard();
 });
 
 /////////collection.js//////////////
