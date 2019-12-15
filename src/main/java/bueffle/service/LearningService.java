214861547e484b2dbff1b424a423e3af406c3fb7
paesc8135 <pascal.schoepfer@students.ffhs.ch>
2019-12-15 22:26:51 +0100

    some javadoc

--- src/main/java/bueffle/service/LearningService.java
+++ src/main/java/bueffle/service/LearningService.java
@@ -1,6 +1,5 @@
 package bueffle.service;
 
-
 import bueffle.db.entity.Card;
 import bueffle.db.entity.CardInLearningRun;
 import bueffle.db.entity.LearningRun;
@@ -12,7 +11,6 @@ import bueffle.model.LearningRunRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Set;
 
 @Service
@@ -30,6 +28,17 @@ public class LearningService {
     @Autowired
     private CardInLearningRunRepository cardInLearningRunRepository;
 
+    /**
+     * Starts a learning run and sets the given collection and the starting user as owner on it and saves it.
+     * It then gets all the cards and makes the relation between the learning run and the cards. The connection is made
+     * by using a ManyToOne relationship to a entity called CardInLearningRun, which stores information about the card
+     * in this learning run. Of course, this entity CardInLearningRun then has a OneToMany with the card entity.
+     * Lastly, fields which would be problematic for the frontend or should not be shown are beeing wiped out and then
+     * the resulting learning run gets returned.
+     * @param learningRun a dummy object which just contains information about the run if it is a normal or plus run
+     * @param collectionId the id of the collection to start the run with
+     * @return the learning run object ready for output with json (with emptied problematic or restricted fields)
+     */
     public LearningRun start(LearningRun learningRun, Long collectionId) {
         learningRun.setCollection(collectionService.findById(collectionId).orElseThrow(
                 () -> new CollectionNotFoundException(collectionId)));
@@ -38,23 +47,40 @@ public class LearningService {
 
         Set<Card> cards = collectionService.findById(collectionId)
                 .orElseThrow(() -> new CollectionNotFoundException(collectionId)).getCards();
-        cards.forEach(card -> {
-            cardInLearningRunRepository.save(
-                    new CardInLearningRun(learningRun, card)
-            );
-        });
+        cards.forEach(card -> {cardInLearningRunRepository.save(new CardInLearningRun(learningRun, card));});
         cards.forEach(Card::emptyRestrictedFields);
         if(resultingLearningRun.getOwner()!= null) {resultingLearningRun.getOwner().emptyRestrictedFields();}
-        return learningRun;
+        return resultingLearningRun;
     }
 
-    public List<LearningRun> getAll() {
-        return learningRunRepository.findAll();
-    }
-
+    /**
+     * Gets the next card of a learning run. First, it finds the learningRun by the provided it and it's related
+     * CardInLearningRuns. Gets the appropriate CardInLearningRun from it's helper method.
+     * Then it increases the shown counter and saves the CardInLearningRun.
+     * Lastly, it gets the associated card and wipes problematic or restricted fields an returns it.
+     * @param learnId the id of the learning run
+     * @return the next card object in this particular run
+     */
     public Card next(Long learnId) {
-        LearningRun learningRun = learningRunRepository.findById(learnId)
-                .orElseThrow(() -> new LearningRunNotFoundException(learnId));
+        CardInLearningRun cardInLearningRun = getCardInLearningRun(learningRunRepository.findById(learnId)
+                .orElseThrow(() -> new LearningRunNotFoundException(learnId)));
+
+        cardInLearningRun.increaseShownCounter();
+        cardInLearningRunRepository.save(cardInLearningRun);
+
+        Card card = cardInLearningRun.getCard();
+        if (card != null) {card.emptyRestrictedFields();}
+        return card;
+    }
+
+    /**
+     * Checks if it's running a normal run or a learning run plus. If it's a plus run, it searches the
+     * CardsInLearningRuns for the first card with it's answer status set to incorrect. If it's a normal
+     * run, it just searches for the first card which has not been shown yet.
+     * @param learningRun the learningRun to get the cardInLearningRun for
+     * @return the resulting CardInLearningRun
+     */
+    private CardInLearningRun getCardInLearningRun(LearningRun learningRun) {
         Set<CardInLearningRun> cardInLearningRuns = learningRun.getCardInLearningRuns();
         CardInLearningRun cardInLearningRun;
         if (learningRun.isLearningRunPlus()) {
@@ -66,13 +92,14 @@ public class LearningService {
                     new CardInLearningRun());
         }
         learningRun.setLastAnsweredCardInLearningRunId(cardInLearningRun.getId());
-        cardInLearningRun.increaseShownCounter();
-        cardInLearningRunRepository.save(cardInLearningRun);
-        Card card = cardInLearningRun.getCard();
-        if (card != null) {card.emptyRestrictedFields();}
-        return card;
+        return cardInLearningRun;
     }
 
+    /**
+     * Sets the answer status of the last shown CardInLearningRun of a learning run.
+     * @param answerStatus boolean status of the answer
+     * @param learnId the id of the learning run to set
+     */
     public void setAnswer(CardInLearningRun answerStatus, Long learnId) {
         LearningRun learningRun = learningRunRepository.findById(learnId)
                 .orElseThrow(() -> new LearningRunNotFoundException(learnId));
